b599eb4431e5105a028abb55d6c1b6c66a5b194e
Klaas Schoute <klaas_schoute@hotmail.com>
2021-08-27 04:09:36 +0200

    Rename the entities

--- test_output.py
+++ test_output.py
@@ -13,6 +13,13 @@ async def main():
     ) as omnik:
         inverter: OmnikInverter = await omnik.inverter()
         print(f"Omnik Inverter: {inverter}")
+        print()
+        print(f"Serial Number: {inverter.serial_number}")
+        print(f"Model: {inverter.model}")
+        print(f"Firmware: {inverter.firmware}")
+        print(f"Current Power: {inverter.current_power}")
+        print(f"Energy Production Today: {inverter.energy_today}")
+        print(f"Energy Production Total: {inverter.energy_total}")
 
 
 if __name__ == "__main__":
