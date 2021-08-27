3d4d18f61a730cc274f518ffd40a6c54f02aa873
Klaas Schoute <klaas_schoute@hotmail.com>
2021-08-27 04:20:27 +0200

    Rename the entities (#9)

--- omnikinverter/models.py
+++ omnikinverter/models.py
@@ -11,11 +11,11 @@ class Inverter:
     """Object representing an Inverter response from Omnik Inverter."""
 
     serial_number: str | None
-    inverter_model: str | None
-    inverter_firmware: str | None
-    inverter_current_power: int | None
-    inverter_energy_today: float | None
-    inverter_energy_total: float | None
+    model: str | None
+    firmware: str | None
+    current_power: int | None
+    energy_today: float | None
+    energy_total: float | None
 
     @staticmethod
     def from_js(data: dict[str, Any]) -> Inverter:
@@ -25,7 +25,7 @@ class Inverter:
             data: The data from the Omnik Inverter.
 
         Returns:
-            A Inverter object.
+            An Inverter object.
         """
 
         def get_values(position):
@@ -41,9 +41,9 @@ class Inverter:
 
         return Inverter(
             serial_number=get_values(0),
-            inverter_model=get_values(3),
-            inverter_firmware=get_values(2),
-            inverter_current_power=get_values(5),
-            inverter_energy_today=get_values(6),
-            inverter_energy_total=get_values(7),
+            model=get_values(3),
+            firmware=get_values(2),
+            current_power=get_values(5),
+            energy_today=get_values(6),
+            energy_total=get_values(7),
         )
