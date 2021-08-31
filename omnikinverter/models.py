3742080094d48c0ecd91a8c6744ea6d5c34d70ea
Klaas Schoute <klaas_schoute@hotmail.com>
2021-09-01 01:08:31 +0200

    Add new data entities to dataclass model (#16)
    * Update data in models
    
    * Add exception for wrong data source
    
    * Add extra tests for pytest

--- omnikinverter/models.py
+++ omnikinverter/models.py
@@ -6,6 +6,8 @@ import re
 from dataclasses import dataclass
 from typing import Any
 
+from .exceptions import OmnikInverterWrongSourceError
+
 
 @dataclass
 class Inverter:
@@ -13,7 +15,9 @@ class Inverter:
 
     serial_number: str | None
     model: str | None
-    firmware: str | None
+    firmware_main: str | None
+    firmware_slave: str | None
+    solar_rated_power: int | None
     solar_current_power: int | None
     solar_energy_today: float | None
     solar_energy_total: float | None
@@ -31,10 +35,12 @@ class Inverter:
 
         data = json.loads(data)
         return Inverter(
-            serial_number=data["g_sn"],
+            serial_number=data["i_sn"],
             model=data["i_modle"],
-            firmware=data["i_ver_m"],
-            solar_current_power=data["i_pow_n"],
+            firmware_main=data["i_ver_m"],
+            firmware_slave=data["i_ver_s"],
+            solar_rated_power=int(data["i_pow"]),
+            solar_current_power=int(data["i_pow_n"]),
             solar_energy_today=float(data["i_eday"]),
             solar_energy_total=float(data["i_eall"]),
         )
@@ -56,17 +62,24 @@ class Inverter:
             else:
                 matches = re.search(r'(?<=myDeviceArray\[0\]=").*?(?=";)', data)
 
-            data_list = matches.group(0).split(",")
-            if position in [5, 6, 7]:
-                if position == 5:
-                    return int(data_list[position])
-                return float(data_list[position]) / 100
-            return data_list[position]
+            try:
+                data_list = matches.group(0).split(",")
+                if position in [4, 5, 6, 7]:
+                    if position in [4, 5]:
+                        return int(data_list[position])
+                    return float(data_list[position]) / 100
+                return data_list[position]
+            except AttributeError as exception:
+                raise OmnikInverterWrongSourceError(
+                    "Your inverter has no data source from a javascript file."
+                ) from exception
 
         return Inverter(
             serial_number=get_values(0),
             model=get_values(3),
-            firmware=get_values(2),
+            firmware_main=get_values(1),
+            firmware_slave=get_values(2),
+            solar_rated_power=get_values(4),
             solar_current_power=get_values(5),
             solar_energy_today=get_values(6),
             solar_energy_total=get_values(7),
