b184a28585b42149c9c0cd9040e891e9a154e9e7
Darsh <darshkpatel@gmail.com>
2022-12-23 18:37:57 +0530

    feat sofar-220tl: refactor test

---
+++ tests/fixtures/status_devicearray_sofar220tl.js
@@ -0,0 +1,1 @@
+var version= "H4.01.51MW.2.01W1.0.64(2018-01-251-D)";var m2mRssi= "39%";var wanIp= "";var myDeviceArray=new Array();myDeviceArray[0]="1234567890  ,V450,,SOFAR2200TL,2 000,400,567,123070,,1,";
